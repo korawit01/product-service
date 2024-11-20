3d4ec800d77efc1545e93cfc525cd6da5f8d2610
Korawit <korawit.r@cliniter.com>
2024-11-20 16:10:48 +0700

    init

---
+++ src/main/java/com/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String>{
+
+}
